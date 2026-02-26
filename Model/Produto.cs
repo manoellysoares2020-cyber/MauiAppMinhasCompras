6b8325fee352794ce457b6fb472d4c35186fa487
Manoelly Soares Alves de França <manoellysoares2020@gmail.com>
2026-02-26 19:23:43 -0300

    criando o básico e construção da classe para fazer CRUD no SQLite

---
+++ Model/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MauiAppMinhasCompras.Model
+{
+    public class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public double Preco { get; set; }
+
+        public Produto() { }
+    }
+}
