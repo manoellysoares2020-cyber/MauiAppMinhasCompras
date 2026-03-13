1ba6aba79e49030eabe0764c1b8cfc3abf0c2e20
Manoelly Soares Alves de França <manoellysoares2020@gmail.com>
2026-03-12 22:04:57 -0300

    Agenda 4 - ListaProduto

--- Views/ListaProduto.xaml.cs
+++ Views/ListaProduto.xaml.cs
@@ -1,12 +1,22 @@
+using MauiAppMinhasCompras.Model;
+using System.Collections.ObjectModel;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
 	public ListaProduto()
 	{
 		InitializeComponent();
+
+		lst_produtos.ItemsSource = lista;
 	}
 
+	protected async override void OnAppearing()
+	{ List<Produto> tmp = await App.Db.GetAll();
+		tmp.ForEach(i => lista.Add(i));
+			}
 	private void ToolbarItem_Clicked(object sender, EventArgs e)
 	{
 		try
